22fdcdf62eac92e9c5b953c22755b13bc062b79a
James D Vieyra <james@vieyra.co.za>
2022-06-01 15:18:28 +0200

    Initial Commit

---
+++ JvieyraSwiftUITestBed/JvieyraSwiftUITestBedApp.swift
@@ -0,0 +1,17 @@
+//
+//  JvieyraSwiftUITestBedApp.swift
+//  JvieyraSwiftUITestBed
+//
+//  Created by IACD-011 on 2022/06/01.
+//
+
+import SwiftUI
+
+@main
+struct JvieyraSwiftUITestBedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
